8b8b8a375ccbfb80e0c116719ad74d3a8ebd1f4d
gaurav7477 <bairagi7477@gmail.com>
2024-02-15 17:57:07 +0530

    Appwrite service and project backend setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import 
+function App() {
+  return (
+    <>
+      <h1>Blog with app write</h1>
+    </>
+  )
+}
+
+export default App
